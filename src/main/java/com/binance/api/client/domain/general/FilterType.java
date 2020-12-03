28b20f82efe9f3f17506eb42a243d44248b7885d
khang <truongnhukhang1993@gmail.com>
2020-12-03 07:12:31 +0700

    update code

--- src/main/java/com/binance/api/client/domain/general/FilterType.java
+++ src/main/java/com/binance/api/client/domain/general/FilterType.java
@@ -6,14 +6,13 @@ package com.binance.api.client.domain.general;
 public enum FilterType {
   // Symbol
   PRICE_FILTER,
-  LOT_SIZE,
-  MIN_NOTIONAL,
   PERCENT_PRICE,
-  MAX_NUM_ORDERS,
-  MAX_ALGO_ORDERS,
-  ICEBERG_PARTS,
-  // Exchange
-  EXCHANGE_MAX_NUM_ORDERS,
-  EXCHANGE_MAX_ALGO_ORDERS,
-  MAX_NUM_ALGO_ORDERS
+      LOT_SIZE,
+  MIN_NOTIONAL,
+      ICEBERG_PARTS,
+  MARKET_LOT_SIZE,
+      MAX_NUM_ORDERS,
+  MAX_NUM_ALGO_ORDERS,
+      MAX_NUM_ICEBERG_ORDERS,
+  MAX_POSITION,
 }
